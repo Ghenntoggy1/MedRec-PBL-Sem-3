c47226b86e5a8a67c0ae510a101fe4715894e682
Ghenntoggy1 <roman.gusev@isa.utm.md>
2023-09-24 15:07:59 +0300

    init commit

---
+++ src/main/java/com/PBLProject/MedRecPBLSem3/MedRecPblSem3Application.java
@@ -0,0 +1,13 @@
+package com.PBLProject.MedRecPBLSem3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedRecPblSem3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedRecPblSem3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/PBLProject/MedRecPBLSem3/MedRecPblSem3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PBLProject.MedRecPBLSem3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedRecPblSem3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
