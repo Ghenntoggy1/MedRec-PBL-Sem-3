52cf493f951563a08ab412fa4f102ef75962e64e
Ghenntoggy1 <roman.gusev@isa.utm.md>
2023-10-11 10:28:26 +0300

    bug-fix: Make button link

--- frontend/src/pages/StartPage.js
+++ frontend/src/pages/StartPage.js
@@ -23,64 +23,64 @@ function StartPage() {
           </Flex>
           <GridItem colSpan={1}>
             <Center>
-              <Button 
-              marginBottom="20px" 
-              variant='ghost'
-              height='210px'
-              border="3px solid transparent"
-              onMouseEnter={() => setIsPacientHovered(true)}
-              onMouseLeave={() => setIsPacientHovered(false)}
-              _hover={{
-                border: '3px solid',
-                borderColor: 'linear(to-l, #4CBCAC, #05C676)',
-                backgroundImage: 'linear-gradient(to-l, #4CBCAC, #05C676)',
-              }}
-              onClick={() => {
-                setUserType('pacient');
-              }}
-              _active={{
-                transform: 'scale(0.95)',
-                borderColor: 'linear-gradient(to-l, #4CBCAC, #05C676)',
-              }}>
-              
-                <ChakraLink as={ReactRouterLink} to={`/login/${'pacient'}`} style={{ textDecoration: 'none' }}> {/* TODO repair userType */}
+              <ChakraLink as={ReactRouterLink} to={`/login/${'pacient'}`} style={{ textDecoration: 'none' }}> {/* TODO repair userType */}
+                <Button 
+                marginBottom="20px" 
+                variant='ghost'
+                height='210px'
+                border="3px solid transparent"
+                onMouseEnter={() => setIsPacientHovered(true)}
+                onMouseLeave={() => setIsPacientHovered(false)}
+                _hover={{
+                  border: '3px solid',
+                  borderColor: 'linear(to-l, #4CBCAC, #05C676)',
+                  backgroundImage: 'linear-gradient(to-l, #4CBCAC, #05C676)',
+                }}
+                onClick={() => {
+                  setUserType('pacient');
+                }}
+                _active={{
+                  transform: 'scale(0.95)',
+                  borderColor: 'linear-gradient(to-l, #4CBCAC, #05C676)',
+                }}>
                   <Flex flexDirection="column" h="197px">
                     <Image
                       src={isPacientHovered ? "/images/pacient_icon_white.png" : "/images/pacient_icon.png"}
                     />
                     <Text fontSize="2xl" fontWeight="bold" bgClip='text' bgGradient={isPacientHovered ? 'linear(to-l, #FFFFFF, #FFFFFF)' : 'linear(to-l, #4CBCAC, #05C47C)'} >Pacient</Text>
                   </Flex>
-                </ChakraLink>
-              </Button>
-              <Button 
-              marginLeft='50px'
-              marginBottom="20px" 
-              variant='ghost'
-              height='210px'
-              border="3px solid transparent"
-              onMouseEnter={() => setIsMedicHovered(true)}
-              onMouseLeave={() => setIsMedicHovered(false)}
-              _hover={{
-                border: '3px solid',
-                borderColor: 'linear(to-l, #4CBCAC, #05C676)',
-                backgroundImage: 'linear-gradient(to-l, #4CBCAC, #05C676)',
-              }}
-              _active={{
-                transform: 'scale(0.95)',
-                borderColor: 'linear-gradient(to-l, #4CBCAC, #05C676)',
-              }}
-              onClick={() => {
-                setUserType('medic');
-              }}>
-                <ChakraLink as={ReactRouterLink} to={`/login/${'medic'}`} style={{ textDecoration: 'none' }}>  {/* TODO repair userType */}
+                
+                </Button>
+              </ChakraLink>
+              <ChakraLink as={ReactRouterLink} to={`/login/${'medic'}`} style={{ textDecoration: 'none' }}>  {/* TODO repair userType */}
+                <Button 
+                marginLeft='50px'
+                marginBottom="20px" 
+                variant='ghost'
+                height='210px'
+                border="3px solid transparent"
+                onMouseEnter={() => setIsMedicHovered(true)}
+                onMouseLeave={() => setIsMedicHovered(false)}
+                _hover={{
+                  border: '3px solid',
+                  borderColor: 'linear(to-l, #4CBCAC, #05C676)',
+                  backgroundImage: 'linear-gradient(to-l, #4CBCAC, #05C676)',
+                }}
+                _active={{
+                  transform: 'scale(0.95)',
+                  borderColor: 'linear-gradient(to-l, #4CBCAC, #05C676)',
+                }}
+                onClick={() => {
+                  setUserType('medic');
+                }}>
                   <Flex flexDirection="column" h="197px">
                     <Image
                       src={isMedicHovered ? "/images/medic_icon_white.png" : "/images/medic_icon.png"}
                     />
                     <Text fontSize="2xl" fontWeight="bold" bgGradient={isMedicHovered ? 'linear(to-l, #FFFFFF, #FFFFFF)' : 'linear(to-l, #4CBCAC, #05C676)'} bgClip='text'>Medic</Text>
                   </Flex>
-                </ChakraLink>
-              </Button>
+                </Button>
+              </ChakraLink>
             </Center>
           </GridItem>
         </Grid>
