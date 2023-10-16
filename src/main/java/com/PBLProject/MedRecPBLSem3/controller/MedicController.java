25de8245936aa58996772c6cb55416f27c074f43
Ghenntoggy1 <roman.gusev@isa.utm.md>
2023-10-16 22:25:14 +0300

    bug-fix: delete unused import in MedicController

--- src/main/java/com/PBLProject/MedRecPBLSem3/controller/MedicController.java
+++ src/main/java/com/PBLProject/MedRecPBLSem3/controller/MedicController.java
@@ -1,7 +1,6 @@
 package com.PBLProject.MedRecPBLSem3.controller;
 
 import com.PBLProject.MedRecPBLSem3.models.Medic;
-import com.PBLProject.MedRecPBLSem3.models.Patient;
 import com.PBLProject.MedRecPBLSem3.repository.MedicRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
 
 @RestController
 public class MedicController {
