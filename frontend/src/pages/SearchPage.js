b1259d5e3cb098920b37826086f1c484989b74e2
Ghenntoggy1 <roman.gusev@isa.utm.md>
2023-12-02 14:03:39 +0200

    Change SearchPage: replaced buttons with checkboxes. Add Search Button. Add validation for letters and numbers

--- frontend/src/pages/SearchPage.js
+++ frontend/src/pages/SearchPage.js
@@ -1,16 +1,14 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { data } from '../api/data';
-import { HStack, Button, Input, Text, GridItem, VisuallyHidden, Link as ChakraLink } from '@chakra-ui/react';
+import { HStack, Button, Checkbox, Input, Text, GridItem, VisuallyHidden, Link as ChakraLink } from '@chakra-ui/react';
 
 export default function SearchPage() {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchType, setSearchType] = useState('name');
   const [isHovered, setIsHovered] = useState(false);
 
-  function Capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
+
 
   const handleInputChange = (event) => {
     setSearchTerm(event.target.value);
@@ -25,7 +23,7 @@ export default function SearchPage() {
   const [message, setMessage] = useState('');
 
   const handleNumbers = event => {
-    const result = event.target.value.replace(/[^a-z]/gi, '');
+    const result = event.target.value.replace(/[^a-z\s]/gi, '');
 
     setMessage(result);
   };
@@ -40,95 +38,97 @@ export default function SearchPage() {
 
       console.log('Search API Response:', response);
 
-      // Handle the response, update the UI, etc.
     } catch (error) {
       console.error(error);
+    }
+  };
+
+  const handleSearchTypeChange = (type) => {
+    setSearchType(type);
+    if (type === 'idnp') {
+      setValue('');
+    } else {
+      setMessage('');
     }
   };
 
   return (
     <div className="main">
       <HStack spacing={4} mb={4}>
-      <Button
-            as={GridItem}
-            variant="solid"
-            colSpan={{
-            base: "auto",
-            lg: 2,
-            }}
-            width="20%"
-            size="lg"
-            type="submit"
-            colorScheme="brand"
-            cursor="pointer"
-            border= '3px solid'
-            borderColor= 'green.300'
-            backgroundImage= 'linear-gradient(to-l, #4CBCAC, #05C676)'
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-            _hover={{
-                border: '3px solid',
-                borderColor: 'gray',
-                backgroundImage: 'linear-gradient(to-l, #4cbcac, #6dd694)',
-            }}
-            _active={{
-                transform: 'scale(0.95)',
-                borderColor: 'gray',
-            }}
-            onClick={() => setSearchType('idnp')}
+        <Checkbox
+          isChecked={searchType === 'idnp'}
+          onChange={() => handleSearchTypeChange('idnp')}
+          colorScheme="blue"
+          cursor="pointer"
+          border="2px solid"
+          borderColor="#4CBCAC"
+          size="lg"
+          paddingLeft="3"
+          paddingRight="3"
         >
-            <Text>Căutare după IDNP</Text>
-        </Button>
+          <Text fontSize="lg">Căutare după IDNP</Text>
+        </Checkbox>
+        <Checkbox
+          isChecked={searchType === 'name'}
+          onChange={() => handleSearchTypeChange('name')}
+          colorScheme="blue"
+          cursor="pointer"
+          border="2px solid"
+          borderColor="#4CBCAC"
+          size="lg"
+          paddingLeft="3"
+          paddingRight="3"
+        >
+          <Text fontSize="lg">Căutare după Nume Prenume</Text>
+        </Checkbox>
+      </HStack>
+      <HStack>
+        <Input
+          mt={0}
+          size="lg"
+          type="search"
+          placeholder={`Search by ${searchType === 'idnp' ? 'IDNP' : 'Name'}`}
+          value={searchType === 'idnp' ? value : message}
+          maxLength= {searchType === 'idnp' ? '13' : '50'}
+          onChange={handleInputChange}
+          onKeyDown={(event) => {
+            if (event.key === 'Enter') {
+              handleSearch();
+            }
+          }}
+          onInput={searchType === 'idnp' ? handleLetters : handleNumbers}
+        />
         <Button
-            as={GridItem}
-            variant="solid"
-            colSpan={{
-            base: "auto",
-            lg: 2,
-            }}
-            width="20%"
-            size="lg"
-            type="submit"
-            colorScheme="brand"
-            cursor="pointer"
-            border= '3px solid'
-            borderColor= 'green.300'
-            backgroundImage= 'linear-gradient(to-l, #4CBCAC, #05C676)'
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-            _hover={{
-                border: '3px solid',
-                borderColor: 'gray',
-                backgroundImage: 'linear-gradient(to-l, #4cbcac, #6dd694)',
-            }}
-            _active={{
-                transform: 'scale(0.95)',
-                borderColor: 'gray',
-            }}
-            onClick={() => setSearchType('name')}
+          as={GridItem}
+          variant="solid"
+          colSpan={{
+          base: "auto",
+          lg: 2,
+          }}
+          width="20%"
+          size="lg"
+          type="submit"
+          colorScheme="brand"
+          cursor="pointer"
+          border= '3px solid'
+          borderColor= 'green.300'
+          backgroundImage= 'linear-gradient(to-l, #4CBCAC, #05C676)'
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          _hover={{
+              border: '3px solid',
+              borderColor: 'gray',
+              backgroundImage: 'linear-gradient(to-l, #4cbcac, #6dd694)',
+          }}
+          _active={{
+              transform: 'scale(0.95)',
+              borderColor: 'gray',
+          }}
+          // onClick={() => data.fetchPatientsByType(searchType)}  // TODO find patients by type
         >
-            <Text>Căutare după Nume Prenume</Text>
+          <Text>Caută</Text>
         </Button>
       </HStack>
-
-      <VisuallyHidden>
-        INDP
-      </VisuallyHidden>
-      <Input
-        mt={0}
-        size="lg"
-        type="search"
-        placeholder={`Search by ${searchType === 'idnp' ? 'IDNP' : 'Name'}`}
-        value={searchType === 'idnp' ? value : message}
-        maxLength= {searchType === 'idnp' ? '13' : '50'}
-        onChange={handleInputChange}
-        onKeyDown={(event) => {
-          if (event.key === 'Enter') {
-            handleSearch();
-          }
-        }}
-        onInput={searchType === 'idnp' ? handleLetters : handleNumbers}
-      />
     </div>
   );
 }
