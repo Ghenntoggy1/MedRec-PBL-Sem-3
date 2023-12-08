ea409a4f10bd08acf521d8af2f3374c2e300ff55
Twisside <136237170+Twisside@users.noreply.github.com>
2023-12-08 20:20:37 +0200

    Update Operatii.js delete coments

--- frontend/src/pages/Operatii.js
+++ frontend/src/pages/Operatii.js
@@ -65,15 +65,6 @@ export default function Operatii(){
     </SimpleGrid>
   )
 }
-
-// import React from 'react';
-// import CustomAccordionItem from './CustomAccordionItem'; // Adjust the import path based on your project structure
-// import { Accordion } from '@chakra-ui/react';
-
-//  const dataFromDatabase = [
-//   // ... array of objects containing denumirea, mediculResponsabil, data, and descrierea properties
-// ];
-
 // const Operatii = () => {
 //   return (
 //     <Accordion allowToggle>
